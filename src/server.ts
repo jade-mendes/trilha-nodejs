ccc84336ea3445f8795a971ceaa813f52373533f
Jade <jdmsa3@gmail.com>
2026-02-13 20:32:59 -0300

    feat: criando e validando variáveis de ambiente

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,11 @@
 import { app } from './app.js'
+import { env } from './env/index.js'
 
-const host = '0.0.0.0'
-const port = 3333
 
 app.listen({
-    host,
-    port
+    host: env.HOST,
+    port: env.PORT,
 }).then(() => {
-    const url = `http://localhost:${port}`
+    const url = `http://localhost:${env.PORT}`
     console.log(`HTTP server running at ${url}`)
 })
