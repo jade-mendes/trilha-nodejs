addac86887c94b6c3e31374e13dd7432e064a40e
Jade <jdmsa3@gmail.com>
2026-02-19 18:25:02 -0300

    feat: criando rota de cadastro de usuário

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,24 @@
 import { fastify } from 'fastify'
+import { z } from 'zod'
+import { prisma } from './libs/prisma.js'
 
 export const app = fastify()
+
+app.post('/users', async(request, reply) => {
+    const registerBodySchema = z.object({
+        name: z.string().trim().min(1).max(100),
+        email: z.email(),
+        password: z.string().min(8).max(100),
+    })
+
+    const { name, email, password } = registerBodySchema.parse(request.body)
+    const user = await prisma.user.create({
+        data: {
+            name,
+            email,
+            passwordHash: password
+        }
+    })
+
+    return reply.status(201).send(user)
+})
