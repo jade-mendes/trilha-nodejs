ea1200af26193fe9250aaa5ed95dc9e6a5f0ddae
Jade <jdmsa3@gmail.com>
2026-02-19 21:49:28 -0300

    refactor: criando constantes para validação + evitam "números mágicos" e repetição de código

---
+++ src/constants/validation-constants.ts
@@ -0,0 +1,4 @@
+export const MIN_PASSWORD_SIZE = 8
+export const MAX_PASSWORD_SIZE = 64
+export const MIN_USERNAME_SIZE = 2
+export const MAX_USERNAME_SIZE = 100
--- src/http/controllers/users/create-user.controller.ts
+++ src/http/controllers/users/create-user.controller.ts
@@ -1,22 +1,28 @@
-import { z } from 'zod'
-import { prisma } from '@/libs/prisma.js'
-import type { FastifyRequest, FastifyReply } from 'fastify'
+import { z } from "zod";
+import { prisma } from "@/libs/prisma.js";
+import type { FastifyRequest, FastifyReply } from "fastify";
+import {
+  MIN_PASSWORD_SIZE,
+  MAX_PASSWORD_SIZE,
+  MIN_USERNAME_SIZE,
+  MAX_USERNAME_SIZE
+} from "@/constants/validation-constants.js";
 
-export async function createUser(request: FastifyRequest, reply: FastifyReply){
-    const registerBodySchema = z.object({
-        name: z.string().trim().min(1).max(100),
-        email: z.email(),
-        password: z.string().min(8).max(100),
-    })
+export async function createUser(request: FastifyRequest, reply: FastifyReply) {
+  const registerBodySchema = z.object({
+    name: z.string().trim().min(MIN_USERNAME_SIZE).max(MAX_USERNAME_SIZE),
+    email: z.email(),
+    password: z.string().min(MIN_PASSWORD_SIZE).max(MAX_PASSWORD_SIZE),
+  });
 
-    const { name, email, password } = registerBodySchema.parse(request.body)
-    const user = await prisma.user.create({
-        data: {
-            name,
-            email,
-            passwordHash: password
-        }
-    })
+  const { name, email, password } = registerBodySchema.parse(request.body);
+  const user = await prisma.user.create({
+    data: {
+      name,
+      email,
+      passwordHash: password,
+    },
+  });
 
-    return reply.status(201).send(user)
+  return reply.status(201).send(user);
 }
