fbdecdd87c91f43f438584c58c87e88693500393
Jade <jdmsa3@gmail.com>
2026-02-20 17:33:11 -0300

    fix: adicionando try-catch no controller de criação de usuário

--- src/http/controllers/users/create-user.controller.ts
+++ src/http/controllers/users/create-user.controller.ts
@@ -4,28 +4,34 @@ import {
   MIN_PASSWORD_SIZE,
   MAX_PASSWORD_SIZE,
   MIN_USERNAME_SIZE,
-  MAX_USERNAME_SIZE
+  MAX_USERNAME_SIZE,
 } from "@/constants/validation-constants.js";
 import { CreateUserUseCase } from "@/use-cases/users/create-user.js";
 import { PrismaUsersRepository } from "@/repositories/prisma/prisma-users-repository.js";
 import { UserPresenter } from "@/http/presenters/user-presenter.js";
-
+import { UserAlreadyExsistsError } from "@/use-cases/errors/user-already-exists-error.js";
 
 export async function createUser(request: FastifyRequest, reply: FastifyReply) {
-  const registerBodySchema = z.object({
-    name: z.string().trim().min(MIN_USERNAME_SIZE).max(MAX_USERNAME_SIZE),
-    email: z.email(),
-    password: z.string().min(MIN_PASSWORD_SIZE).max(MAX_PASSWORD_SIZE),
-  });
+  try {
+    const registerBodySchema = z.object({
+      name: z.string().trim().min(MIN_USERNAME_SIZE).max(MAX_USERNAME_SIZE),
+      email: z.email(),
+      password: z.string().min(MIN_PASSWORD_SIZE).max(MAX_PASSWORD_SIZE),
+    });
 
-  const { name, email, password } = registerBodySchema.parse(request.body);
-  
-  const usersRepository = new PrismaUsersRepository()
-  const { user } = await new CreateUserUseCase(usersRepository).execute({
-    name,
-    email,
-    password
-  })
+    const { name, email, password } = registerBodySchema.parse(request.body);
 
-  return reply.status(201).send(UserPresenter.toHTTP(user));
+    const usersRepository = new PrismaUsersRepository();
+    const { user } = await new CreateUserUseCase(usersRepository).execute({
+      name,
+      email,
+      password,
+    });
+
+    return reply.status(201).send(UserPresenter.toHTTP(user));
+  } catch (error) {
+    if (error instanceof UserAlreadyExsistsError) {
+      return reply.status(409).send({message: error.message})
+    }
+  }
 }
