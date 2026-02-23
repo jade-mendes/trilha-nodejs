0b9387160b02c3055457a898a1fa4b6fbf96ecde
Jade <jdmsa3@gmail.com>
2026-02-23 18:37:11 -0300

    style: formatação

--- src/http/controllers/users/update-user.controller.ts
+++ src/http/controllers/users/update-user.controller.ts
@@ -3,7 +3,10 @@ import { UserPresenter } from "@/http/presenters/user-presenter.js";
 import z from "zod";
 import { ResourceNotFoundError } from "@/use-cases/errors/resource-not-found-error.js";
 import { makeUpdateUserUseCase } from "@/use-cases/factories/make-update-user-use-case.js";
-import { MAX_USERNAME_SIZE, MIN_USERNAME_SIZE } from "@/constants/validation-constants.js";
+import {
+  MAX_USERNAME_SIZE,
+  MIN_USERNAME_SIZE,
+} from "@/constants/validation-constants.js";
 
 export async function updateUser(request: FastifyRequest, reply: FastifyReply) {
   try {
@@ -14,18 +17,23 @@ export async function updateUser(request: FastifyRequest, reply: FastifyReply) {
     const { publicId } = getParamsSchema.parse(request.params);
 
     const updateBodySchema = z.object({
-          name: z.string().trim().min(MIN_USERNAME_SIZE).max(MAX_USERNAME_SIZE).optional(),
-          email: z.email().optional(),
-        });
-    
-        const { name, email } = updateBodySchema.parse(request.body);
+      name: z
+        .string()
+        .trim()
+        .min(MIN_USERNAME_SIZE)
+        .max(MAX_USERNAME_SIZE)
+        .optional(),
+      email: z.email().optional(),
+    });
+
+    const { name, email } = updateBodySchema.parse(request.body);
 
     const updateUserUseCase = makeUpdateUserUseCase();
 
     const { user } = await updateUserUseCase.execute({
       publicId,
       name,
-      email
+      email,
     });
     return reply.status(200).send(UserPresenter.toHTTP(user));
   } catch (error) {
